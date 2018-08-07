cd95f37ffe1fefec7926a9c1ef0a21707a68ede4
learn <eric.desimone@cortland.edu>
2018-08-07 07:44:46 -0700

    more recipe_show work

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -11,4 +11,10 @@ module RecipesHelper
    ]
   end
 
+  def ingredients(recipe)
+    recipe.recipe_ingredients.collect do |r_i|
+      "#{r_i.ingredient.type_name}: #{r_i.ingredient.name} #{pluralize(r_i.quantity, "#{r_i.measurement.capitalize}") unless r_i.ingredient.type_name == "Yeast"}"
+    end
+  end
+
 end
