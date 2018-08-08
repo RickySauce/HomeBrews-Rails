244d6502c8a5aa874fda0f4245136a0f075677c3
learn <eric.desimone@cortland.edu>
2018-08-08 06:19:53 -0700

    more functionality for recipe show view

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   get '/logout' => 'sessions#destroy'
   resources :users
   resources :users, only: [:show] do
-    resources :recipes, only: [:show, :index, :edit, :new, :update, :destroy, :create]
+    resources :recipes, only: [:show, :index, :edit, :new, :update, :create]
   end
-  resources :recipes, only: [:show, :index, :create, :destroy, :update]
+  resources :recipes, only: [:show, :index, :create, :update]
+  delete '/recipes/:id' => 'recipes#destroy'
   resources :sub_styles, only: [:index, :new, :create, :show]
   resources :sub_styles, only: [:show] do
     resources :recipes, only: [:index, :show]
